d29b0625a618900966b3b478df0201d9dff9ba87
KimonePrem <Kimone.Premlall@investec.co.za>
2020-08-28 15:29:17 +0200

    feature: splash screen added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashScreenComponent,
+    HomeScreenComponent
   ],
   imports: [
     BrowserModule
