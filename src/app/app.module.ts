d2de4c07989b26d5c731c26859e53b2b5b342279
KimonePrem <Kimone.Premlall@investec.co.za>
2020-10-08 20:58:25 +0200

    feature: change accent colour to dark blue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { SplashScreenComponent } from './splash-screen/splash-screen.component';
-import { HomeScreenComponent } from './home-screen/home-screen.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+
+import {AppComponent} from './app.component';
+import {SplashScreenComponent} from './splash-screen/splash-screen.component';
+import {HomeScreenComponent} from './home-screen/home-screen.component';
+import {SideNavbarComponent} from './side-navbar/side-navbar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SignUpComponent} from './sign-up/sign-up.component';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     SplashScreenComponent,
-    HomeScreenComponent
+    HomeScreenComponent,
+    SideNavbarComponent,
+    SignUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MDBBootstrapModule.forRoot(),
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+  signInForm: FormGroup;
+  constructor() {
+    this.signInForm = new FormGroup({
+      emailFormControl: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      passwordFormControl: new FormControl('', [
+        Validators.minLength(4),
+        Validators.required])
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
